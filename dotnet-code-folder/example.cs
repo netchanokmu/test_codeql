fde74b91f2d7de2eb2dd8070c35a0507fe116293
pimncn01 <netchanok.m@cloudnatician.com>
2023-11-21 19:51:41 +0700

    test dotnet 2

--- dotnet-code-folder/example.cs
+++ dotnet-code-folder/example.cs
@@ -1,69 +1,12 @@
-// File: Example.cs
-
 using System;
-using System.Data.SqlClient;
 
-class Example
+class Program
 {
-    static void VulnerableFunction(string userInput)
-    {
-        // Vulnerable SQL query, userInput is directly concatenated into the query
-        string dangerousQuery = "SELECT * FROM users WHERE username = '" + userInput + "'";
-        ExecuteQuery(dangerousQuery);
-    }
-
-    static void SafeFunction(string userInput)
-    {
-        // Safer SQL query, using parameterized query to prevent SQL injection
-        string safeQuery = "SELECT * FROM users WHERE username = @Username";
-        ExecuteQueryWithParameter(userInput, safeQuery);
-    }
-
-    static void ExecuteQuery(string query)
-    {
-        using (SqlConnection connection = new SqlConnection("YourConnectionString"))
-        {
-            connection.Open();
-            using (SqlCommand command = new SqlCommand(query, connection))
-            {
-                using (SqlDataReader reader = command.ExecuteReader())
-                {
-                    while (reader.Read())
-                    {
-                        // Process the result set
-                    }
-                }
-            }
-        }
-    }
-
-    static void ExecuteQueryWithParameter(string userInput, string query)
-    {
-        using (SqlConnection connection = new SqlConnection("YourConnectionString"))
-        {
-            connection.Open();
-            using (SqlCommand command = new SqlCommand(query, connection))
-            {
-                // Add parameter to prevent SQL injection
-                command.Parameters.AddWithValue("@Username", userInput);
-
-                using (SqlDataReader reader = command.ExecuteReader())
-                {
-                    while (reader.Read())
-                    {
-                        // Process the result set
-                    }
-                }
-            }
-        }
-    }
-
     static void Main()
     {
-        Console.Write("Enter username: ");
-        string userInput = Console.ReadLine();
+        Console.WriteLine("Hello! What's your name?");
+        string name = Console.ReadLine();
 
-        VulnerableFunction(userInput);
-        SafeFunction(userInput);
+        Console.WriteLine($"Nice to meet you, {name}!");
     }
 }
