06c93cb164209003bbbe5bec5a250feb75476000
pimncn01 <netchanok.m@cloudnatician.com>
2023-11-22 16:09:43 +0700

    test 002

--- python-code-folder/example.py
+++ python-code-folder/example.py
@@ -1,13 +1,17 @@
-# example_code.py
+# hardcoded_secret.py
 
-def process_data(user_data):
-    # Example of a potentially insecure print statement
-    print("User data:", user_data)
+def send_request(api_key, data):
+    # Simulate sending a request with the API key
+    print(f"Sending request with API key: {api_key}")
+    print(f"Data: {data}")
 
 def main():
-    user_input = input("Enter sensitive data: ")
-    process_data(user_input)
+    # Example of a potentially insecure hardcoded API key
+    api_key = "your_hardcoded_api_key"
+    user_data = input("Enter sensitive data: ")
+
+    # Send a request with the hardcoded API key and user data
+    send_request(api_key, user_data)
 
 if __name__ == "__main__":
     main()
-
